dfde13049ead903929c78f882a7b37b5a704af16
Rali <rayanedu92290@gmail.com>
2025-04-23 02:22:51 +0200

    add text

--- app/page.tsx
+++ app/page.tsx
@@ -6,5 +6,10 @@ export default function Hello() {
   const { data } = useSWR("/api/hello", (url) =>
     api.get(url).then((r) => r.data)
   );
-  return <pre className="p-4">{data ? data.message : "Loading…"}</pre>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <pre className="p-4">{data ? data.message : "Loading…"}</pre>;
+    </div>
+  );
 }
